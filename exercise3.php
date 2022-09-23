9347e08667f30248e1e25a0bec29ab325cfc802e
reyLeeAn <113161547+reyLeeAn@users.noreply.github.com>
2022-09-23 12:50:28 +0800

    Add files via upload

---
+++ exercise3.php
@@ -0,0 +1,17 @@
+<?php 
+
+ $x=8; 
+
+ echo "Value is now " . $x . "\n";
+ $x += 2;
+ echo "Value is now " . $x . "\n";
+ $x -= 4;
+ echo "Value is now " . $x . "\n";
+ $x *= 5;
+ echo "Value is now " . $x . "\n";
+ $x /= 3;
+ echo "Value is now " . $x . "\n";
+ $x = ++$x;
+ echo "Value is now " . $x . "\n";
+ $x = --$x;
+ echo "Value is now " . $x;

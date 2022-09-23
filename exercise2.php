ffb4fc8001b20d89dbb6ea73952f7893a767ae07
reyLeeAn <113161547+reyLeeAn@users.noreply.github.com>
2022-09-23 12:46:44 +0800

    Add files via upload

---
+++ exercise2.php
@@ -0,0 +1,16 @@
+<?php
+
+$x = 10;
+$y = 7;
+
+$sum = $x + $y;
+$difference = $x - $y;
+$product = $x * $y;
+$quotient = $x / $y;
+$modulo = $x % $y;
+
+echo "$x + $y = " . $sum . "\n";
+echo "$x - $y = " . $difference . "\n";
+echo "$x * $y = " . $product . "\n";
+echo "$x / $y = " . $quotient . "\n";
+echo "$x % $y = " . $modulo;

cca5396c5d320fafc8e6d27b37161a2960846836
reyLeeAn <113161547+reyLeeAn@users.noreply.github.com>
2022-09-23 12:51:45 +0800

    Add files via upload

---
+++ exercise4.php
@@ -0,0 +1,16 @@
+<?php   
+ 
+$name='Harry'; 
+$age=28;   
+ 
+var_dump($name); 
+echo "\n";   
+ 
+print_r($name); 
+echo "\n";   
+ 
+var_dump($age); 
+echo "\n";  
+  
+$name=NULL;   
+var_dump($name);
